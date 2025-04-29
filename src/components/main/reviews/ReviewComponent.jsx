2e92d4536e7d17477552dda89acaad2eb1e75e28
bananc4ik <rkononcuk03@gmail.com>
2025-04-29 14:47:56 +0300

    A

---
+++ src/components/main/reviews/ReviewComponent.jsx
@@ -0,0 +1,20 @@
+import react from "react";
+import ReviewComponentStyles from './ReviewComponent.css'
+
+
+const ReviewComponent = ({review}) =>{
+    return(
+
+        <div>
+
+            <div className="images"></div> 
+            <div className="user_name">
+                <p></p>
+                <img src="" alt="" />   
+            </div>
+
+            <div className="reviews_description"></div>           
+
+        </div>
+    )
+}
