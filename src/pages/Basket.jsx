a027a2441c5c6b1b99596e6854da13e3df04afe3
vladkuznetsov2901 <valdkuznetsov2901@gmail.com>
2025-07-27 14:48:42 +0300

    MY_CHANGES17

--- src/pages/Basket.jsx
+++ src/pages/Basket.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import react, { useState } from "react";
 import Header from "../components/header/Header";
 import Header2 from "../components/header/Header2";
 import BasketStyle from '../styles/basket.css'
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "../app/productSlice";
 import { useEffect } from "react";
 import ProductComponent from "../components/main/products/ProductComponent";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 
 
@@ -16,9 +16,13 @@ const Basket = () => {
 
     const productsFromApi = useSelector((state) => state.products.productsList);
 
-    let productsBusket = [];
+    
+  const [productsBasket, setProductsBasket] = useState([]);
 
 
+
+    const busketItems = localStorage.getItem("productIds")
+    console.log(busketItems);
 
     useEffect(() => {
         dispatch(fetchProducts());
@@ -26,53 +30,50 @@ const Basket = () => {
     }, [dispatch]);
 
     useEffect(() => {
+        if (busketItems != null) setProductsBasket(productsFromApi.filter(product => busketItems.includes(product.id)));
 
-        productsBusket = productsFromApi.filter(product => busketItems.includes(product.id))
-
-    }, [productsFromApi])
-
+    }, [productsFromApi]);
 
 
 
 
 
-    const busketItems = localStorage.getItem("productIds")
-    console.log(busketItems);
 
 
     return (
 
         <div className="container-fluid" >
 
-            <div className="row">
-
-            </div>
-
             <Header />
             <Header2 />
 
-            <div className="basket_box">
-
-                <p className="basket_title">Your cart</p>
-
-                <div className="basket_products">
-
-                    {productsBusket.map((product) => (
-
-                        <Link style={{ textDecoration: "none" }} className="col-lg-3 col-8" to={`/item_detail/${product.id}`}><ProductComponent key={product.id} product={product} /></Link>
-
-                    ))}
-                </div>
-
-                <div className="basket_options">
+            <div className="row">
 
 
+
+
+                <div className="basket_box">
+
+                    <p className="basket_title">Your cart</p>
+
+                    <div className="basket_products">
+
+                        {productsBasket.map((product) => (
+
+                            <Link style={{ textDecoration: "none" }} className="col-lg-3 col-8" to={`/item_detail/${product.id}`}><ProductComponent key={product.id} product={product} /></Link>
+
+                        ))}
+                    </div>
+
+                    <div className="basket_options">
+
+
+
+                    </div>
 
                 </div>
 
             </div>
-
-
 
         </div>
 
